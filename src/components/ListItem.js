a602e5f3b5ada0aadaddbb97070caad120f7eeda
Anita Ilieva <anita.ilieva@acando.no>
2018-02-28 15:39:40 +0100

    Add styling to the list

---
+++ src/components/ListItem.js
@@ -0,0 +1,24 @@
+import React, { Component } from 'react';
+import { Text } from 'react-native';
+import { connect } from 'react-redux';
+import { CardSection } from './common';
+
+class ListView extends Component {
+	render() {
+		const { titleStyle } = styles;
+		return (
+			<CardSection>
+				<Text style={titleStyle}>{this.props.library.title}</Text>
+			</CardSection>
+		);
+	}
+}
+
+const styles = {
+		titleStyle: {
+			fontSize: 18,
+			paddingLeft: 15
+		}	
+}
+
+export default ListView;
